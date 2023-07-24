0eb010dc78910f177a8f873eb9fcf2300a41739e
uehrlich <uriehrlich@gmail.com>
2023-07-24 16:58:05 +0300

    first

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <QGuiApplication>
+#include <QUrl>
+
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
+    return app.exec();
+}
